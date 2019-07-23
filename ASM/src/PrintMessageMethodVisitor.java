f6d9ec1ac3b8beb8e0a90b58ffc0b5f948c36c92
zuloloxi <zuloloxi@users.noreply.github.com>
2019-07-23 10:44:12 +0300

    Ziye Xing nice working code

--- ASM/src/PrintMessageMethodVisitor.java
+++ ASM/src/PrintMessageMethodVisitor.java
@@ -7,10 +7,12 @@ import org.objectweb.asm.Opcodes;
 public class PrintMessageMethodVisitor extends MethodVisitor {
 	private boolean isImportant = false;
 	ArrayList<String> parameterIndexes;
+	//private String methodName;
 
 	public PrintMessageMethodVisitor(MethodVisitor mv, String name, String className) {
 		super(Opcodes.ASM5, mv);
 		parameterIndexes = new ArrayList<>();
+		//methodName = name;
 	}
 
 	@Override
@@ -38,6 +40,8 @@ public class PrintMessageMethodVisitor extends MethodVisitor {
 	@Override
 	public void visitCode() {
 		super.visitCode();
+		//System.out.println(methodName);
+		//Ziye Xing adding code (bytecode) that working nice
 		if (isImportant) {
 			for (String index : parameterIndexes) {
 				mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
@@ -45,5 +49,29 @@ public class PrintMessageMethodVisitor extends MethodVisitor {
 				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false);
 			}
 		}
+		/*
+		starting older code not working...
+		if (isImportant) {
+			mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+			mv.visitLdcInsn(parameterIndexes.toString());
+			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+			/*
+			mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", 
+			        "out","Ljava/io/PrintStream;");
+			mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder");
+			mv.visitInsn(Opcodes.DUP);
+			
+			mv.visitLdcInsn("A call was made to method \"");
+		    mv.visitMethodInsn(Opcodes.INVOKESPECIAL,
+		        "java/lang/StringBuilder", "",
+		        "(Ljava/lang/String;)V", false);
+		    mv.visitLdcInsn(methodName);
+		    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+		        "java/lang/StringBuilder", "append",
+		        "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+		    */
+		}
+	 	ending older code not working
+		*/
 	}
 }
